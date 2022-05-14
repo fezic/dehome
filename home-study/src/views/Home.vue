88d105f1418c953e708ae03454a35ccd3d56386c
fezic <nauthyhh@outlook.com>
2022-05-14 10:43:36 +0800

    初始

---
+++ home-study/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>demo</div>
+    <h1>我是master主内容</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
